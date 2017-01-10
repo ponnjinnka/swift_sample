dbaab00743101722d4670657d7224f4d22658597
ponnjinnka <penponnnnjinnka@gmail.com>
2017-01-10 23:16:20 +0900

    first commit

--- MySample/ViewController.swift
+++ MySample/ViewController.swift
@@ -12,8 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("ありがとう")
+        
+        let msg1 = "こんにちは"
+        let msg2 = "サンキュー"
+        let msg3 = "さようなら"
+        print(msg1, msg2, msg3, separator:"/" )
+        
+        let tax = 0.08
+        print(tax)
+        
+        kuji()
+        }
+    
+    func kuji() {
+        let num = arc4random_uniform(10)+1
+        if num>=7 {
+            let msg = "あたり"
+            print(num, msg)
+        }else{
+            let msg = "はずれ"
+            print(num, msg)
+            
+        }
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

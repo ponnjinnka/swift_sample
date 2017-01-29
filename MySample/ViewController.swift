4e1caecbc1dca705299b4a4cf606c0b4c1d4730b
ponnjinnka <penponnnnjinnka@gmail.com>
2017-01-30 00:10:27 +0900

    add random button

--- MySample/ViewController.swift
+++ MySample/ViewController.swift
@@ -10,5 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var ok: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myLabel = UILabel()
+        myLabel.text = "ハローワールド"
+        
+        myLabel.frame = CGRect(x: 50, y: 100, width: 200, height: 21)
+        
+        myLabel.textColor = UIColor.blue
+        myLabel.backgroundColor = UIColor.lightGray
+        
+        view.addSubview(myLabel)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func random(_ sender: Any) {
+        let num = arc4random_uniform(1000)
+        label.text = String(num)
+        
+        ok.isEnabled = (num>=200)
+    }
+    
+    @IBAction func tapOk(_ sender: Any) {
+        print("OK")
+    }
 }
 
